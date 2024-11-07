a1660533c56ad98e815b1e874ddc7e581b4e640b
Wojciech Gałek <wgalek24@wp.pl>
2024-11-07 14:26:28 +0100

    Create middleware CheckUserRole

---
+++ app/Http/Middleware/CheckUserRole.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Enums\UserRole;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckUserRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = $request->user();
+        if($user && $user->role() === UserRole::ADMIN) {
+            return $next($request);
+        }
+
+        return response()->json(['error' => 'Unauthorized'], 403);
+    }
+}
